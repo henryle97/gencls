2cb1a2368538283a6450ea5b9a8b4675835f5112
LeHoang <lehoang2797@gmail.com>
2022-03-11 14:50:06 +0700

    add color aug

--- gencls/dataset/preprocess/aug.py
+++ gencls/dataset/preprocess/aug.py
@@ -9,22 +9,21 @@ class ImgAugTransform:
 
         self.aug = iaa.Sequential(iaa.SomeOf((1, 5),
                                              [
-            # blur
-
+            # blurW
             sometimes(iaa.OneOf([iaa.GaussianBlur(sigma=(0, 1.0)),
                                 iaa.MotionBlur(k=3)])),
 
             # color
-            # sometimes(iaa.AddToHueAndSaturation(
-            #     value=(-10, 10), per_channel=True)),
-            # sometimes(iaa.SigmoidContrast(gain=(3, 10),
-            #           cutoff=(0.4, 0.6), per_channel=True)),
-            # sometimes(iaa.Invert(0.25, per_channel=0.5)),
-            # sometimes(iaa.Solarize(0.5, threshold=(32, 128))),
-            # sometimes(iaa.Dropout2d(p=0.5)),
-            # sometimes(iaa.Multiply((0.5, 1.5), per_channel=0.5)),
-            # sometimes(iaa.Add((-40, 40), per_channel=0.5)),
-            # sometimes(iaa.JpegCompression(compression=(5, 80))),
+            sometimes(iaa.AddToHueAndSaturation(
+                value=(-10, 10), per_channel=True)),
+            sometimes(iaa.SigmoidContrast(gain=(3, 10),
+                      cutoff=(0.4, 0.6), per_channel=True)),
+            sometimes(iaa.Invert(0.25, per_channel=0.5)),
+            sometimes(iaa.Solarize(0.5, threshold=(32, 128))),
+            sometimes(iaa.Dropout2d(p=0.5)),
+            sometimes(iaa.Multiply((0.5, 1.5), per_channel=0.5)),
+            sometimes(iaa.Add((-40, 40), per_channel=0.5)),
+            sometimes(iaa.JpegCompression(compression=(5, 80))),
 
             # distort
             sometimes(iaa.Crop(percent=(0.01, 0.03),
